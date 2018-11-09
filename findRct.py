366d5059a1411bf51cedba7b6d88d2a27639e727
leranlily <leranlily@163.com>
2018-11-09 12:43:43 +0800

    Update findRct.py

--- findRct.py
+++ findRct.py
@@ -7,6 +7,7 @@ height = 5.5
 width = 8.5
 ratio = width/height
 
+#calculate the angle of three points
 def angle(d1,d2,d3):
     x1,y1,x2,y2,x3,y3 = d1[0,0],d1[0,1],d2[0,0],d2[0,1],d3[0,0],d3[0,1]
     
@@ -16,6 +17,7 @@ def angle(d1,d2,d3):
     B=math.degrees(math.acos((b*b-a*a-c*c)/(-2*a*c)))
     return a, c, B
 
+#calculate the forth point with three points in rectangle
 def point4(d1,d2,d3):
     x1,y1,x2,y2,x3,y3 = d1[0,0],d1[0,1],d2[0,0],d2[0,1],d3[0,0],d3[0,1]
     x4 = x1+x3-x2
@@ -23,35 +25,33 @@ def point4(d1,d2,d3):
     d4 = [[x4,y4]]
     return d4
 
-img = cv2.imread("6.jpg")
-img = cv2.GaussianBlur(img,(3,3),0)  
+img = cv2.imread("1.jpg")
+img = cv2.GaussianBlur(img,(3,3),0)  # gaussian filtering
 
-gray = cv2.cvtColor(img,cv2.COLOR_BGR2GRAY)
 h, w = img.shape[:2]
 if w > MAX_WIDTH:
 	resize_rate = MAX_WIDTH / w
 	img = cv2.resize(img, (MAX_WIDTH, int(h*resize_rate)), interpolation=cv2.INTER_AREA)
 h, w, channels = img.shape
-MIN_AREA = h*w/10
-cv2.imshow("Origin",img)
+MIN_AREA = h*w/100
+
+#floodfill
 blured = cv2.blur(img,(5,5))
-cv2.imshow("Blur", blured)
 mask = np.zeros((h+2, w+2), np.uint8)
 cv2.floodFill(blured, mask, (w-1,h-1), (255,255,255), (2,2,2),(3,3,3),8)
-cv2.imshow("floodfill", blured)  
 gray = cv2.cvtColor(blured,cv2.COLOR_BGR2GRAY)  
-cv2.imshow("gray", gray)  
-kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(50, 50))
 
+#openning and closing
+kernel = cv2.getStructuringElement(cv2.MORPH_RECT,(50, 50))
 opened = cv2.morphologyEx(gray, cv2.MORPH_OPEN, kernel)  
 closed = cv2.morphologyEx(opened, cv2.MORPH_CLOSE, kernel)  
-cv2.imshow("closed", closed)  
-canny = cv2.Canny(closed, 50, 150)
-ret, binary = cv2.threshold(closed,250,255,cv2.THRESH_BINARY)  
-cv2.imshow("binary", binary)  
-image, contours, hierarchy = cv2.findContours(canny,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)   
-length = len(contours)
-#cv2.drawContours(img, contours, -1, (0, 0, 255), 1)
+
+#canny detect edge
+img_edge = cv2.Canny(closed, 50, 150)
+
+image, contours, hierarchy = cv2.findContours(img_edge,cv2.RETR_TREE,cv2.CHAIN_APPROX_SIMPLE)   
+
+#find rectangle in contours
 for i in range(length):
     cnt = contours[i]
     epsilon = 0.02 * cv2.arcLength(cnt,True)
@@ -68,7 +68,7 @@ for i in range(length):
                     cv2.polylines(img, [np.array([approx[k],approx[(k+1)%len(approx)],approx[(k+2)%len(approx)],d4],np.int32)], True, (0, 0, 255), 1)
                     break
 
-cv2.imwrite("r7.jpg",img) 
+
 cv2.imshow("approx",img)
 cv2.waitKey(0)
 cv2.destroyAllWindows() 
